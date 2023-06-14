f2fd81b9827795bfaef5ea47150ad221d71090e7
a00269002_AndreaGaviria <a00269002@mycambrian.ca>
2023-06-14 17:17:42 -0400

    PitRoom test

--- MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
+++ MinotaurLabyrinthTest/MinotaurLabyrinthTest.cs
@@ -1,14 +1,27 @@
 ﻿using MinotaurLabyrinth;
-
 namespace MinotaurLabyrinthTest
 {
     [TestClass]
     public class Tests
     {
         [TestMethod]
-        public void DummyTest()
+        public void PitRoomTest()
         {
-            Assert.AreNotSame(1, 2);
+            Pit pitRoom = new Pit();
+            Hero hero = new Hero();
+            Map map = new Map(1, 1);
+            pitRoom.Activate(hero, map);
+            Assert.AreEqual(pitRoom.IsActive, false);
+            Assert.AreEqual(hero.IsAlive, true);
+
+            hero.HasSword = true;
+            pitRoom.Activate(hero, map);
+            // Hero should not die because pitRoom is inactive here
+            Assert.AreEqual(hero.IsAlive, true);
+
+            Pit newPitRoom = new Pit();
+            newPitRoom.Activate(hero, map);
+            Assert.AreEqual(hero.IsAlive, false);
         }
     }
 }
